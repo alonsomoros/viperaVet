f72fafde23b3d834797fd8b54d490acbb33bb73f
Alon <alonso.moros@alumnos.upm.es>
2025-10-06 13:00:25 +0200

    Service básico de CRUD para los controllers posteriormente

---
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/service/UserService.java
@@ -0,0 +1,15 @@
+package com.alonso.vipera.training.springboot_apirest.service;
+
+import java.util.List;
+
+import com.alonso.vipera.training.springboot_apirest.model.User;
+
+public class UserService {
+    public List<User> getAll(){
+        return null;
+    }
+
+    public User getById(Long id){
+        return null;
+    }
+}
