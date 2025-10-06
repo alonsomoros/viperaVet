c57fb9f961cd035404d55e0f2cd73f30562e9317
Alon <alonso.moros@alumnos.upm.es>
2025-10-06 13:57:53 +0200

    Cambiado User para implementar en la BBDD

--- src/main/java/com/alonso/vipera/training/springboot_apirest/model/User.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/model/User.java
@@ -1,5 +1,11 @@
 package com.alonso.vipera.training.springboot_apirest.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +15,19 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
     private String password;
+
+    @Column(nullable = false, unique = true)
     private String email;
 }
