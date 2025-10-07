c68c484cc84ae6cb56971690526168902fbbf7d4
Alon <alonso.moros@alumnos.upm.es>
2025-10-07 11:34:34 +0200

    Añadido Builder a User para parsear entre UserInDTO

--- src/main/java/com/alonso/vipera/training/springboot_apirest/model/User.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/model/User.java
@@ -7,16 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@Data
+@Builder
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
