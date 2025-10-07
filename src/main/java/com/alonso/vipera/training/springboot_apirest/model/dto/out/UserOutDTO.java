a1c7b6f928165cc4faa08677bb90f81ef6cbffd4
Alon <alonso.moros@alumnos.upm.es>
2025-10-07 10:54:23 +0200

    Corregido por mantenibilidad

--- src/main/java/com/alonso/vipera/training/springboot_apirest/model/dto/out/UserOutDTO.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/model/dto/out/UserOutDTO.java
@@ -2,18 +2,19 @@ package com.alonso.vipera.training.springboot_apirest.model.dto.out;
 
 import com.alonso.vipera.training.springboot_apirest.model.User;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class UserOutDTO {
     private String username;
     private String email;
+
+    public UserOutDTO(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
 
     public static UserOutDTO toDTO(User user) {
         return new UserOutDTO(user.getUsername(), user.getEmail());
