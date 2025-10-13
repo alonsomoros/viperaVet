f0a29069bbd27fb1530b8c9793d7034c2482cccc
Alon <alonso.moros@alumnos.upm.es>
2025-10-13 13:20:48 +0200

    Creados controladores para responsabilidades

---
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.alonso.vipera.training.springboot_apirest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alonso.vipera.training.springboot_apirest.model.userDto.in.LoginRequestDTO;
+import com.alonso.vipera.training.springboot_apirest.model.userDto.in.RegisterRequestDTO;
+import com.alonso.vipera.training.springboot_apirest.model.userDto.out.AuthResponseDTO;
+import com.alonso.vipera.training.springboot_apirest.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterRequestDTO registerRequestDto) {
+        AuthResponseDTO authResponseDto = authService.register(registerRequestDto);
+        return ResponseEntity.ok(authResponseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDto) {
+        AuthResponseDTO authResponseDto = authService.login(loginRequestDto);
+        return ResponseEntity.ok(authResponseDto);
+    }
+}
