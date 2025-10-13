26a2002c95afd650b20a45edee769cfb50f6c656
Alon <alonso.moros@alumnos.upm.es>
2025-10-13 13:49:47 +0200

    arreglo 3

--- src/main/java/com/alonso/vipera/training/springboot_apirest/controller/AuthController.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.alonso.vipera.training.springboot_apirest.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
