cae6a369321e7f2587533fbdd5ea6337185bd7dd
Alon <alonso.moros@alumnos.upm.es>
2025-10-17 10:06:02 +0200

    Añadida Documentación de Swagger a AuthController

--- src/main/java/com/alonso/vipera/training/springboot_apirest/controller/AuthController.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/controller/AuthController.java
@@ -11,21 +11,40 @@ import com.alonso.vipera.training.springboot_apirest.model.user.dto.in.RegisterR
 import com.alonso.vipera.training.springboot_apirest.model.user.dto.out.AuthResponseDTO;
 import com.alonso.vipera.training.springboot_apirest.service.AuthService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Auth", description = "API endpoints for managing the authentication process")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Registro de Usuario", description = "Permite un usuario registrarse en el sistema y obtener un token de autenticación.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario registrado con éxito", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Contraseña débil. La contraseña debe tener al menos 6 caracteres.", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Formato de email inválido.", content = @Content),
+            @ApiResponse(responseCode = "409", description = "'X' parámetro está ya está en uso", content = @Content)
+    })
     @PostMapping("/register")
     public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterRequestDTO registerRequestDto) {
         AuthResponseDTO authResponseDto = authService.register(registerRequestDto);
         return ResponseEntity.ok(authResponseDto);
     }
 
+    @Operation(summary = "Login de Usuario", description = "Permite un usuario loggearse en el sistema y obtener un token de autenticación.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario loggeado con éxito", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponseDTO.class))),
+            @ApiResponse(responseCode = "401", description = "Credenciales de entrada inválidas", content = @Content)
+    })
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDto) {
         AuthResponseDTO authResponseDto = authService.login(loginRequestDto);
