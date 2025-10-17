85f1416524aa7268bbe37d47c8ae343aa26892e2
Alon <alonso.moros@alumnos.upm.es>
2025-10-17 09:39:07 +0200

    Añadida Documentación de Swagger a PetController

--- src/main/java/com/alonso/vipera/training/springboot_apirest/controller/PetController.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/controller/PetController.java
@@ -1,5 +1,7 @@
 package com.alonso.vipera.training.springboot_apirest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +17,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alonso.vipera.training.springboot_apirest.model.pet.dto.in.PetInDTO;
+import com.alonso.vipera.training.springboot_apirest.model.pet.dto.out.PetOutDTO;
 import com.alonso.vipera.training.springboot_apirest.service.PetService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/pets")
+@Tag(name = "Pets", description = "API endpoints for managing pets")
 @RequiredArgsConstructor
 public class PetController {
 
@@ -29,14 +40,26 @@ public class PetController {
 
     // GET calls
 
+    @Operation(summary = "Obtener mis mascotas", description = "Devuelve una lista de todas las mascotas que pertenecen al usuario autenticado a través de su token JWT.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de mascotas encontradas con éxito", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PetOutDTO.class)))),
+            @ApiResponse(responseCode = "403", description = "Acceso denegado. Se necesita un token válido", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Token no válido o expirado", content = @Content)
+    })
     @GetMapping("/my-pets")
-    public ResponseEntity<?> getMyPets(@AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<List<PetOutDTO>> getMyPets(@AuthenticationPrincipal UserDetails userDetails) {
         String username = userDetails.getUsername();
         return ResponseEntity.status(HttpStatus.OK).body(petService.getPetsByOwnerUsername(username));
     }
 
+    @Operation(summary = "Buscar mascotas con filtros", description = "Devuelve una lista de mascotas que coinciden con los parámetros de búsqueda proporcionados. Si no se proporcionan parámetros, devuelve todas las mascotas.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mascotas encontradas con éxito", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(type = "array", implementation = PetOutDTO.class)))),
+            @ApiResponse(responseCode = "403", description = "Acceso denegado. Se necesita un token válido", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Token no válido o expirado", content = @Content)
+    })
     @GetMapping("/search")
-    public ResponseEntity<?> searchPets(
+    public ResponseEntity<List<PetOutDTO>> searchPets(
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String breed,
             @RequestParam(required = false) String specie) {
@@ -57,14 +80,27 @@ public class PetController {
 
     // POST calls
 
+    @Operation(summary = "Registrar una mascota", description = "Permite registrar una mascota asociada al usuario autenticado a través de su token JWT.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mascota registrada con éxito", content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = PetOutDTO.class))),
+            @ApiResponse(responseCode = "403", description = "Acceso denegado. Se necesita un token válido", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Token no válido o expirado", content = @Content)
+    })
     @PostMapping("/register")
-    public ResponseEntity<?> registerPet(@AuthenticationPrincipal UserDetails userDetails,
+    public ResponseEntity<PetOutDTO> registerPet(@AuthenticationPrincipal UserDetails userDetails,
             @RequestBody PetInDTO petInDTO) {
         String username = userDetails.getUsername();
         return ResponseEntity.status(HttpStatus.OK).body(petService.save(petInDTO, username));
     }
 
-    // DEETE calls
+    // DELETE calls
+
+    @Operation(summary = "Borrar una mascota", description = "Permite borrar una mascota asociada al usuario autenticado a través de su token JWT.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mascota borrada con éxito", content = @Content()),
+            @ApiResponse(responseCode = "403", description = "Acceso denegado. Se necesita un token válido", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Token no válido o expirado", content = @Content)
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletePet(@PathVariable Long id) {
         petService.delete(id);
