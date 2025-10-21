6b01b77f4d9f9f2ad3c75a1e829d53e127ed3752
Alon <alonso.moros@alumnos.upm.es>
2025-10-21 10:08:07 +0200

    Cambiado nombre por conveniencia

--- src/main/java/com/alonso/vipera/training/springboot_apirest/model/dogBreedAPI/dto/DogApiBreedDTO.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/model/dogBreedAPI/dto/DogApiBreedDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class DogBreedDTO {
+public class DogApiBreedDTO {
     private Long id;
     private String name;
     private String bred_for;
