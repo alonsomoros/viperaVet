4325a6bbc3f5a4f9ffd807e7a4c4f176f4d662f3
Alon <alonso.moros@alumnos.upm.es>
2025-10-21 10:14:21 +0200

    Correccion Lexica

--- src/main/java/com/alonso/vipera/training/springboot_apirest/model/pet/Specie.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/model/pet/Specie.java
@@ -32,7 +32,7 @@ public class Specie {
     @OneToMany(mappedBy = "specie")
     private Set<Breed> breeds;
 
-    public SpecieOutDTO tDtoO() {
+    public SpecieOutDTO tDto() {
         return new SpecieOutDTO(this.id, this.name);
     }
 
