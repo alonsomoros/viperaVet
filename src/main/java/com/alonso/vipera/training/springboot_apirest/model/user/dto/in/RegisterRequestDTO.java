ec090b420f9745094fc8168f4793aca415048824
Alon <alonso.moros@alumnos.upm.es>
2025-10-23 11:07:41 +0200

    Modificados mensajes en Register DTOs de entrada

--- src/main/java/com/alonso/vipera/training/springboot_apirest/model/user/dto/in/RegisterRequestDTO.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/model/user/dto/in/RegisterRequestDTO.java
@@ -16,26 +16,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class RegisterRequestDTO {
-    @NotBlank
-    @Size(min = 3, max = 50)
+    @NotBlank(message = "Nombre de usuario necesario")
+    @Size(min = 3, max = 50, message = "Nombre de usuario debe tener entre 3 y 50 caracteres")
     private String username;
 
-    @NotBlank
-    @Size(min = 6, max = 100)
+    @NotBlank(message = "Contraseña necesaria")
+    @Size(min = 6, max = 100, message = "Contraseña debe tener entre 6 y 100 caracteres")
     private String password;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "Email necesario")
+    @Email(message = "Email no válido")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Teléfono necesario")
     @Pattern(regexp = "^\\+?[0-9 .()\\-]{7,25}$", message = "Invalid phone number")
     private String phone;
 
-    @NotBlank
-    @Size(max = 200)
+    @Size(min = 3, max = 100, message = "La dirección debe tener entre 3 y 100 caracteres")
     private String address;
 
-    @NotNull
+    @NotNull(message = "Rol necesario")
     private User.Role role;
 }
