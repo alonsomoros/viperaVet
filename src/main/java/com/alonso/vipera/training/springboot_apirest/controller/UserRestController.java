08b7669bdcd497fcd0e686fad10c7377911dcd86
Alon <alonso.moros@alumnos.upm.es>
2025-11-05 12:50:43 +0100

    V2. Añadido Update de User [Arreglado]

--- src/main/java/com/alonso/vipera/training/springboot_apirest/controller/UserRestController.java
+++ src/main/java/com/alonso/vipera/training/springboot_apirest/controller/UserRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alonso.vipera.training.springboot_apirest.model.user.User.Role;
@@ -25,7 +26,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
