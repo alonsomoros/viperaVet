820d70f26853aebfb6f6b7ef08512ece12210977
Alon <alonso.moros@alumnos.upm.es>
2026-01-18 23:17:05 +0100

    Mejorado Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,15 @@ const Home = () => {
     const { user, logout } = useAuth();
     const navigate = useNavigate();
 
+    const animalImages = [
+    'https://images.unsplash.com/photo-1663546874330-f3967787418d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx2ZXRlcmluYXJ5JTIwYW5pbWFscyUyMHBldHN8ZW58MXx8fHwxNzY4NjA2NjU5fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+    'https://images.unsplash.com/photo-1609348490161-a879e4327ae9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxoYXBweSUyMGRvZyUyMHBvcnRyYWl0fGVufDF8fHx8MTc2ODUzMjYzNnww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+    'https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjYXQlMjBwZXR8ZW58MXx8fHwxNzY4NTY4NjM3fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+    'https://images.unsplash.com/photo-1695510864636-38ff5ba5a945?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxyYWJiaXQlMjBwZXR8ZW58MXx8fHwxNzY4NTM2NzE5fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+    'https://images.unsplash.com/photo-1699721770122-c60db6612c44?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxiaXJkJTIwcGFycm90JTIwcGV0fGVufDF8fHx8MTc2ODYwNjY2MHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+    'https://images.unsplash.com/photo-1548412342-98d0d2a49205?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxoYW1zdGVyJTIwcGV0fGVufDF8fHx8MTc2ODU1Mzc2MHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+  ];
+
     const handleLogout = () => {
         logout();
         navigate('/login');
@@ -14,18 +23,27 @@ const Home = () => {
 
     return (
         <div className="home-container">
-            <div className="home-card">
-                <h1>Bienvenido</h1>
-                {user ? (
-                    <div className="user-info">
-                        <p className="welcome-message">Te has logueado como <strong>{user.role}</strong></p>
-                        <p className="user-detail">Usuario: {user.username}</p>
-                        <button onClick={handleLogout} className="logout-button">Cerrar Sesión</button>
-                    </div>
-                ) : (
-                    <p>Cargando información del usuario...</p>
-                )}
+            <div className="user-info">
+                <p className="welcome-message">Bienvenido <strong>{user.username}</strong> con rol: <strong>{user.role}</strong></p>
             </div>
+
+            
+            <div className="animal-images-container"> 
+                {animalImages.map((image, index) => (
+              <div
+                key={index}
+                className="animal-image"
+              >
+                <img
+                  src={image}
+                  alt={`Animal ${index + 1}`}
+                  className="animal-image"
+                />
+              </div>
+            ))}
+            </div>
+
+            <button onClick={handleLogout} className="logout-button">Cerrar Sesión</button>
         </div>
     );
 };
